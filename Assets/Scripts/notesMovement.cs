64b6027bec05bcd20704a1c0fbca6e5f9f0ed2de
evangelospot <e.potamianos@students.uu.nl>
2022-06-08 17:03:37 +0200

    notesMovement and notesRenderer scripts added (distanceTime variables set to 2s)

--- Assets/Scripts/notesMovement.cs
+++ Assets/Scripts/notesMovement.cs
@@ -6,17 +6,26 @@ public class notesMovement : MonoBehaviour
 {
     [SerializeField] Transform CubeTarget;
     [SerializeField] float speed;
+    [SerializeField] float distanceTimeMovement;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
 
     }
 
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 a = transform.position;
-        Vector3 b = CubeTarget.position;
-        transform.position = Vector3.MoveTowards(a, b, speed);
+        if (Time.time >= distanceTimeMovement)
+        {
+            Vector3 a = transform.position;
+            Vector3 b = CubeTarget.position;
+            transform.position = Vector3.MoveTowards(a, b, speed);
+        }
+        
     }
 }
